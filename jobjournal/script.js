9992b4b67e6ac55f64a5fd71de3093045884d82c
badefan <imdesperateaf123@gmail.com>
2023-08-24 19:44:17 +0300

    Added more days

--- jobjournal/script.js
+++ jobjournal/script.js
@@ -3,7 +3,7 @@ const back = document.querySelector("#back")
 const daytitle = document.querySelector("#day")
 const entry = document.querySelector("#entry")
 
-let pages = ["I got to open up some phones, disconnect the batteries and then throw them in the pile. A couple tablets too. I didnt do much else but near the end of the day I also uninstalled an antivirus off a customers computer with some help."]
+let pages = ["I got to open up some phones, disconnect the batteries and then throw them in the pile, A couple tablets too. I didnt do much else but near the end of the day I also uninstalled an antivirus off a customers computer with some help.", "More phone opening for them to be recycled. I also got to take some hard drives out of some stuff but slow day otherwise.", "I opened a single tablet the entire day, sometimes there just isn't work to do.", "I tested a customers hard drive on their PC because it was supposed to have a problem, but I didnt find anything. After that, I was fixing a printer by using the cleaning software thing and then testing if the ink was coming out properly, but I didnt quite get it fully cleaned before having to leave so I didn't miss my bus"]
 
 let day = 0
 
