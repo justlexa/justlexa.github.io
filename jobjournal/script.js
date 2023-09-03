b548dddd62680127d15ebc79d6fd2316ffb294a0
justlexa <94641097+justlexa@users.noreply.github.com>
2023-09-03 20:07:38 +0300

    Update script.js

--- jobjournal/script.js
+++ jobjournal/script.js
@@ -9,7 +9,10 @@ let pages = ["I got to open up some phones, disconnect the batteries and then th
 "I tested a customers hard drive on their PC because it was supposed to have a problem, but I didnt find anything. After that, I was fixing a printer by using the cleaning software thing and then testing if the ink was coming out properly, but I didnt quite get it fully cleaned before having to leave so I didn't miss my bus",
 "I setup a pc and a laptop for the company, doing stuff like downloading windows updates and drivers aswell as using Ninite to setup a bunch of useful applications easily.",
 "More work trying to sort out a printer for most of the day. I didnt end up getting it working and somehow running the printer cleaning stuff made it work even worse than it was before. I didnt do much else before that.",
-"There was literally no work for me to do so I ended up going home early since there was no point in me just sitting around doing nothing. There was some talk about me getting to try and improve a company program, which im interested in. They have to wipe the customer data first though, so I haven't got to see it yet."]
+"There was literally no work for me to do so I ended up going home early since there was no point in me just sitting around doing nothing. There was some talk about me getting to try and improve a company program, which im interested in. They have to wipe the customer data first though, so I haven't got to see it yet.",
+"Once again had barely any work, and went home early.",
+"I set up a laptop again, went home early.",
+"I set up 2 laptops with windows updates and Ninite, not much else. Might get my hands on a program for setting up customers, without any customer data of course."]
 
 let day = 0
 
