cbe612295a712e07fca7d7fd27b817393eadf74b
justlexa <94641097+justlexa@users.noreply.github.com>
2023-10-25 08:06:12 +0300

    codig

---
+++ OTcoding/kertausjs2.js
@@ -0,0 +1,1 @@
+document.getElementById("viikonpaiva").innerHTML = "Tänään on tiistai"
---
+++ OTcoding/kertausjs3.js
@@ -0,0 +1,7 @@
+
+const button = document.getElementById("painike")
+button.addEventListener("click", show)
+
+function show() {
+    document.getElementById("viikonpaiva").innerHTML = "Tänään on tiistai"
+}
